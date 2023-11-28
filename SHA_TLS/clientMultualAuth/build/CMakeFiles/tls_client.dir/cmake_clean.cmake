c618839809626ae27c90ecab14f3fd2c5fbe1e48
dedog54 <mofanguo54@gmail.com>
2023-11-28 16:30:53 +0100

    Using multithreaded

---
+++ SHA_TLS/clientMultualAuth/build/CMakeFiles/tls_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tls_client.dir/main.cpp.o"
+  "tls_client"
+  "tls_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tls_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
